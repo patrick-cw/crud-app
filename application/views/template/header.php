065bccda36dee68753759ca594de688772e6a013
patrick-cw <patrick1528@gmail.com>
2021-06-15 04:20:08 +0700

    update

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Starter Template for Bootstrap</title>
+    <title>CRUD App</title>
 	<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>assets/css/bootstrap-datepicker3.min.css" rel="stylesheet">
 	<script src="<?php echo base_url(); ?>assets/js/jquery-3.5.0.min.js"></script>
